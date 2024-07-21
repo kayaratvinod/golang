1cc41df7e731296e2bc29eaadb9bfe624adfaacc
kayaratvinod <vinod.balakrishnan@gmail.com>
2024-07-21 01:25:59 -0500

    Updated

--- math.go
+++ math.go
@@ -1,13 +1,11 @@
 package math
 
-import "testing"
+// Add is our function that sums two integers
+func Add(x, y int) (res int) {
+	return x + y
+}
 
-func TestAdd(t *testing.T){
-
-    got := Add(4, 6)
-    want := 10
-
-    if got != want {
-        t.Errorf("got %q, wanted %q", got, want)
-    }
+// Subtract subtracts two integers
+func Subtract(x, y int) (res int) {
+	return x - y
 }
