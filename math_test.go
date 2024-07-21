f5c8343f6daad718165c477cd67fd042189f4fc4
kayaratvinod <vinod.balakrishnan@gmail.com>
2024-07-21 03:46:51 -0500

    Updated

--- math_test.go
+++ math_test.go
@@ -1,39 +1,44 @@
 package math
+import "testing"
 
-import (
-    "fmt"
-    "testing"
-)
+
 
 // arg1 means argument 1 and arg2 means argument 2, and the expected stands for the 'result we expect'
 type addTest struct {
     arg1, arg2, expected int
 }
 
+type subtractTest struct {
+    arg1, arg2, expected int
+}
+
 var addTests = []addTest{
-    addTest{2, 3, 5},
-    addTest{4, 8, 12},
-    addTest{6, 9, 15},
-    addTest{3, 10, 13},
+    {2, 3, 5},
+    {4, 8, 12},
+    {6, 9, 15},
+    {3, 10, 13},
+}
+
+var subtractTests = []subtractTest{
+    {5, 3, 2},
+    {8, 4, 4},
+    {15, 9, 6},
+    {13, 10, 3},
 }
 
 func TestAdd(t *testing.T) {
-
     for _, test := range addTests {
         if output := Add(test.arg1, test.arg2); output != test.expected {
-            t.Errorf("Output %q not equal to expected %q", output, test.expected)
+            t.Errorf("Output %d not equal to expected %d", output, test.expected)
         }
     }
 }
 
-func BenchmarkAdd(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        Add(4, 6)
+func TestSubtract(t *testing.T) {
+    for _, test := range subtractTests {
+        if output := Subtract(test.arg1, test.arg2); output != test.expected {
+            t.Errorf("Output %d not equal to expected %d", output, test.expected)
+        }
     }
 }
 
-
-func ExampleAdd() {
-    fmt.Println(Add(4, 6))
-    // Output: 10
-}
